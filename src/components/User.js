a66df1702b381704947e0b66513bec6fcbc8285a
Chris <christopherpack17@gmail.com>
2025-04-03 22:15:07 -0500

    attempt to make netlify work

--- src/components/User.js
+++ src/components/User.js
@@ -7,22 +7,25 @@ function UpdateUserData({ uid, displayName, email, photoURL }) {
   if (!uid) return null;
 
   const userRef = firebase.database().ref(`users/${uid}`);
-  return userRef
-    .set({
-      uid,
-      name: displayName || 'User',
-      email,
-      photoURL,
-      lastLogin: Date.now(),
-    })
-    .then(() => {
-      const emailEncoded = btoa(email);
-      const emailMapRef = firebase.database().ref(`emailToUid/${emailEncoded}`);
-      return emailMapRef.set(uid);
-    })
-    .catch((error) => {
-      console.error('Error updating user data:', error);
-    });
+  return (
+    userRef
+      .set({
+        uid,
+        name: displayName || 'User',
+        email,
+        photoURL,
+        lastLogin: Date.now(),
+      })
+      // If I want the option to look up someone by their email later:
+      // .then(() => {
+      //   const emailEncoded = btoa(email);
+      //   const emailMapRef = firebase.database().ref(`emailToUid/${emailEncoded}`);
+      //   return emailMapRef.set(uid);
+      // })
+      .catch((error) => {
+        console.error('Error updating user data:', error);
+      })
+  );
 }
 
 UpdateUserData.propTypes = {
